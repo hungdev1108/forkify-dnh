ec782162d990b6e6a663907cb0649b1912c08163
hungdev1108 <user.name>
2022-11-01 12:26:20 +0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '6afc9465-3d80-4a98-b4bd-46ef3c1b1295';
+
+export const MODAL_CLOSE_SEC = 2.5;
